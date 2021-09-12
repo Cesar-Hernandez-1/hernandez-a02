2c844e8ad3f9489ad9fa65121c429bc509d0ff87
Cesar-Hernandez-1 <cesarhernandez@Knights.ucf.edu>
2021-09-12 19:08:53 -0400

    added exercise18 Date: 9/12/2021

---
+++ exercise18/src/main/java/exercise18/Solution18.java
@@ -0,0 +1,59 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Cesar Hernandez
+ */
+
+/*
+ *  Pseudocode:
+ *  Ask user to enter an upper or lower case F or C.
+ *
+ *  If user entered F, prompt for Celsius temperature.
+ *
+ *  If user entered C, prompt for Fahrenheit temperature.
+ *
+ *  Prompt user to enter temperature in Fahrenheit or Celsius to convert to
+ *      other respective metric.
+ *
+ *  Calculate respective conversion:
+ *      C = (F − 32) × 5 / 9
+ *          or
+ *      F = (C × 9 / 5) + 32
+ *      Where C is the temperature in celsius and F is the temperature in Fahrenheit.
+ *    with the given input value and the right equation requested.
+ *
+ *  Output the temperature conversion in the other temperature metric.
+ */
+
+package exercise18;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Solution18 {
+
+    public static double calculateTemperature(double temp, String tempChoice){
+        if(tempChoice.contains("Fahrenheit")){
+            return (temp - 32) * 5 / 9;
+        }else{
+            return (temp * 9 / 5) + 32;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String tempPrompt = "he temperature in ";
+
+        System.out.print("Press C to convert from Fahrenheit to Celsius.\nPress F to convert from Celsius to Fahrenheit.\nYour choice: ");
+
+        if(Objects.equals(input.nextLine().toUpperCase(Locale.ROOT), "C")){
+            tempPrompt += "Fahrenheit";
+        }else{
+            tempPrompt += "Celsius";
+        }
+
+        System.out.print("Please enter t" + tempPrompt + ": ");
+        System.out.printf("T%s is %f.", tempPrompt, calculateTemperature(input.nextDouble(),tempPrompt));
+
+    }
+}
